683caa3f5a59827045dbaa0b7c4d807df66ab95f
xDasn <dajana.snopkova@gmail.com>
2021-10-19 11:55:15 +0200

    back to timer as a component

--- scripts/a-frame-components.js
+++ scripts/a-frame-components.js
@@ -18,9 +18,12 @@ if (image.complete && image.naturalHeight !== 0) {
 	if (training == true) {
 		scene = "tr_" + scene;
 	};
-}
+	var clock=document.getElementById("timer").components.timer;
+        clock.start();
+	}
 });
 
+/*
 var time = 0;
 var myVar = setInterval(counter, 10);
 
@@ -64,38 +67,40 @@ function counter (){
 		});
 	}
 };
+*/	
 
-	/*	
-  AFRAME.registerComponent('timer', {
+AFRAME.registerComponent('timer', {
 	schema: {
 		TimeOutTime : {type:'int', default:5 }
 	},
 	init: function () {
 		this.gotonext = true;
-		var data = this.data;
 		var sceneEl = document.querySelector('a-scene');
 		var date= new Date();
-    	this.TargetTime = date.getTime() + data.TimeOutTime*1000;
-		sceneEl.querySelector("#timer").setAttribute('text', {width: 1.5, height: 1.5, align: 'center', color: 'red', value: '' + toString(data.TimeOutTime)}, true);
+		this.TimeOutTime = this.data.TimeOutTime;
+    	this.TargetTime = date.getTime() + this.TimeOutTime*1000;
+		sceneEl.querySelector("#timer").setAttribute('text', {width: 1.5, height: 1.5, align: 'center', color: 'red', value: '5:00 s     ' +  taskCount + '/36'}, true);
+		this.paused= true;
 	},
 	TimeLeft:function(){
         let CurrentDate = new Date();
 		CurrentDate = CurrentDate.getTime();
 		this.wholeTimeRemaining = this.TargetTime - CurrentDate;
+		/*
 		let timeRemaining = this.TargetTime - CurrentDate;
 		if (timeRemaining >= 0) {	
 			this.seconda = parseInt(timeRemaining / 1000);
 			timeRemaining = (timeRemaining % 1000);
 			this.miliseconda = parseInt(timeRemaining);
-    	};
+    	};*/
     },
 	tick: function () {
-		if (this.wholeTimeRemaining > 0 || this.wholeTimeRemaining == undefined) {
+		console.log(this.wholeTimeRemaining);
+		if ((this.paused==false && this.wholeTimeRemaining > 0) || (this.paused==false && this.wholeTimeRemaining == undefined)) {
 		    this.TimeLeft();
 			var sceneEl = document.querySelector('a-scene');
-			sceneEl.querySelector("#timer").setAttribute('text', {width: 1.5, height: 1.5, align: 'center', color: 'red', value: msToTime(this.wholeTimeRemaining)}, true);
-		} else {
-			if (this.gotonext == true) {
+			sceneEl.querySelector("#timer").setAttribute('text', {width: 1.5, height: 1.5, align: 'center', color: 'red', value: (this.wholeTimeRemaining/1000).toFixed(2)}, true);
+		} else if (this.paused==false && this.gotonext == true) {
 				responses.push([scene, userId, null, null, null, null, null]);
 				$.when(
 					saveDataToExistingFile(responsesFile, arrayToCSV(responses)),
@@ -112,12 +117,16 @@ function counter (){
 						window.open("010_questionnaire.html?taskOr=" + taskOr + "&taskCount=" + taskCount + "&userId=" + userId, "_self");
 					}
 				});
+				this.gotonext = false; //prevent from loading nextPage each frame
 			}
-			this.gotonext = false; //prevent from loading nextPage each frame
-			}
-		}
-  });*/
-
+		},
+	start: function () {
+		var date= new Date();
+		this.TargetTime = date.getTime() + this.TimeOutTime*1000;
+		this.paused = false;
+	}
+  });
+  
 AFRAME.registerComponent('clickhandler', {
 	schema: {
 	  txt: {default:'default'}
@@ -126,6 +135,10 @@ AFRAME.registerComponent('clickhandler', {
 	  var data = this.data;
 	  var el = this.el;
 	  el.addEventListener('click', function (evt) {
+		var time = document.getElementById('timer').getAttribute('text');
+		var timeS = time["value"].substring(0,1);
+		var timeMs = time["value"].substring(2);
+		var time = parseInt(timeS)*1000 + parseInt(timeMs);
 		responses.push([scene, userId, time, data.txt, evt.detail.intersection.point.x, evt.detail.intersection.point.y, evt.detail.intersection.point.z]);
 		if ((data.txt == "corridor_left") || (data.txt == "corridor_right")) {
 			$.when(
@@ -149,6 +162,10 @@ AFRAME.registerComponent('clickhandler', {
 
 AFRAME.registerComponent('rotation-reader', {
 	tick: function () {
+		var time = document.getElementById('timer').getAttribute('text');
+		var timeS = time["value"].substring(0,1);
+		var timeMs = time["value"].substring(2);
+		var time = parseInt(timeS)*1000 + parseInt(timeMs);
 		interaction.push([scene, userId, time, "rotate", this.el.object3D.rotation.x, this.el.object3D.rotation.y]);
 	}
 });
